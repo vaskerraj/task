33605ac705ede93844142143de2476a938f8a182
Dharma Raj Bhandari <yohungdha@gmail.com>
2023-07-28 17:06:21 +1000

    create function to check allowed paramters

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -1,4 +1,21 @@
+const checkAllowedParams = (x, y) => {
+  const allowedParams = ['string', 'number', 'boolen', 'object', 'undefined'];
+
+  if (!allowedParams.includes(typeof (x)) && !allowedParams.includes(typeof (y)))
+    throw new Error("Invalide parameter(s). Please provide string, number, boolen, object, undefined and null as argument.");
+
+  // check array
+  if (Array.isArray(x) || Array.isArray(y))
+    throw new Error('Invalide parameter(s). Please provide string, number, boolen, object, undefined and null as argument.');
+
+  return true;
+}
+
+
 const deepEqual = (a, b) => {
+  if (checkAllowedParams(a, b)) {
+    return true;
+  }
   return false;
 }
 
