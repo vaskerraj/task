ab0e395ab2b51e10f785dc2f2f77af400d184a14
Dharma Raj Bhandari <yohungdha@gmail.com>
2023-07-28 17:42:32 +1000

    only check array to throw error

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -1,13 +1,7 @@
 const checkAllowedParams = (x, y) => {
-  const allowedParams = ['string', 'number', 'boolen', 'object', 'undefined'];
-
-  if (!allowedParams.includes(typeof (x)) && !allowedParams.includes(typeof (y)))
-    throw new Error("Invalide parameter(s). Please provide string, number, boolen, object, undefined and null as argument.");
-
   // check array
   if (Array.isArray(x) || Array.isArray(y))
-    throw new Error('Invalide parameter(s). Please provide string, number, boolen, object, undefined and null as argument.');
-
+    throw new Error('Invalid parameter(s). Paramaters cant be an array!');
   return true;
 }
 
@@ -24,10 +18,9 @@ const deepEqual = (a, b) => {
   if (checkAllowedParams(a, b)) {
     if (typeof (a) === 'object' || typeof (b) === 'object') {
       if (!checkEmptyObj(a) && !checkEmptyObj(b)) {
-        
-      }else{
-        return false;
+
       }
+      return false;
     } else {
       return a === b ? true : false;
     }
